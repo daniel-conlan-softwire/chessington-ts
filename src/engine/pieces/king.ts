0515d95c74b4ade9f6f938689ac4ce2f10114df6
Bianca <bianca.darolti@softwire.com>
2022-09-30 15:09:56 +0100

    the king can move now yay

--- src/engine/pieces/king.ts
+++ src/engine/pieces/king.ts
@@ -1,6 +1,8 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import GameSettings from '../gameSettings';
+import Square from '../square';
 
 export default class King extends Piece {
     public constructor(player: Player) {
@@ -8,6 +10,20 @@ export default class King extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const availableMoves = new Array();
+        const currentPosition = board.findPiece(this);
+        const currentRow = currentPosition.row;
+        const currentCol = currentPosition.col;
+
+
+        const offsets = [1, 0, -1];
+
+        for (let x of offsets) for (let y of offsets) {
+            const nextSquare = new Square(currentRow + x, currentCol + y);
+            if (!board.squareOccupied(nextSquare)) {
+                availableMoves.push(nextSquare);
+            }
+        }
+        return availableMoves;
     }
 }
