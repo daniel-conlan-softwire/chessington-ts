9ee1871b62f91448972dcb2408dbf1c621d41b2c
daniel-conlan-softwire <Daniel.Conlan@softwire.com>
2023-06-28 12:08:10 +0100

    Bishops can now take pieces. Discrimination.

--- src/engine/pieces/bishop.ts
+++ src/engine/pieces/bishop.ts
@@ -3,6 +3,7 @@ import Player from '../player';
 import Board from '../board';
 import GameSettings from '../gameSettings';
 import Square from '../square';
+import SquareState from '../squareState';
 
 export default class Bishop extends Piece {
     public constructor(player: Player) {
@@ -16,11 +17,31 @@ export default class Bishop extends Piece {
         // Everywhere
         for (let leftMult of [1, -1]) for (let rightMult of [1, -1]) {
             for (let offset = 1; offset < GameSettings.BOARD_SIZE; offset++) {
-                const nextSquare = new Square(currentPosition.row + offset*leftMult, currentPosition.col + offset*rightMult);
+                const nextSquare = new Square(currentPosition.row + offset * leftMult, currentPosition.col + offset * rightMult);
 
-                if (!board.squareOccupied(nextSquare)) {
-                    availableMoves.push(nextSquare);
-                } else break;
+                let earlyBreak = false;
+
+                switch (board.squareState(nextSquare)) {
+                    case SquareState.White:
+                        if (this.player === Player.BLACK) availableMoves.push(nextSquare);
+                        earlyBreak = true;
+                        break;
+                    case SquareState.Black:
+                        if (this.player === Player.WHITE) availableMoves.push(nextSquare);
+                        earlyBreak = true;
+                        break;
+                    case SquareState.OutOfBounds:
+                        earlyBreak = true; break;
+                    case SquareState.Free:
+                        availableMoves.push(nextSquare);
+                        break;
+                    case SquareState.King:
+                        earlyBreak = true;
+                        break;
+                }
+
+                if (earlyBreak) break;
+
             }
         }
 
