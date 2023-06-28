ffeba514bc384c5ac31b12839d747e9d2b843221
daniel-conlan-softwire <Daniel.Conlan@softwire.com>
2023-06-28 11:16:37 +0100

    knights can move now yay

--- src/engine/pieces/knight.ts
+++ src/engine/pieces/knight.ts
@@ -1,6 +1,7 @@
 import Piece from './piece';
 import Player from '../player';
 import Board from '../board';
+import Square from '../square';
 
 export default class Knight extends Piece {
     public constructor(player: Player) {
@@ -8,6 +9,26 @@ export default class Knight extends Piece {
     }
 
     public getAvailableMoves(board: Board) {
-        return new Array(0);
+        const offsets = [
+            [1, 2],
+            [2, 1],
+            [-1, 2],
+            [2, -1],
+            [-2, 1],
+            [1, -2],
+            [-1, -2],
+            [-2, -1],
+        ];
+
+        const current = board.findPiece(this);
+        const availableMoves = new Array();
+
+        for (let [x, y] of offsets) {
+            const candidatePosition = new Square(current.row + y, current.col + x);
+
+            availableMoves.push(candidatePosition);
+        }
+
+        return availableMoves;
     }
 }
