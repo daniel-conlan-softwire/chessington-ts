215d07de55ddc33488bc675d6fc4a1bf0c1a64fa
daniel-conlan-softwire <Daniel.Conlan@softwire.com>
2023-06-28 12:18:39 +0100

    pawns now have guns

--- src/engine/pieces/pawn.ts
+++ src/engine/pieces/pawn.ts
@@ -3,6 +3,7 @@ import Player from '../player';
 import Board from '../board';
 import Square from '../square';
 import GameSettings from '../gameSettings';
+import SquareState from '../squareState';
 
 export default class Pawn extends Piece {
     public constructor(player: Player) {
@@ -19,9 +20,9 @@ export default class Pawn extends Piece {
 
         if (!board.squareOccupied(nextPosition)) {
             availableMoves.push(nextPosition);
-            
+
             const row = this.player === Player.WHITE ? 1 : GameSettings.BOARD_SIZE - 2;
-    
+
             if (currentPosition.row === row) {
                 const doubleMovePosition = new Square(nextPosition.row + moveDirection, currentPosition.col);
                 if (!board.squareOccupied(doubleMovePosition)) {
@@ -30,6 +31,22 @@ export default class Pawn extends Piece {
             }
         }
 
+        for (let x of [-1, 1]) {
+            const nextSquare = new Square(currentPosition.row + moveDirection, currentPosition.col + x);
+
+            switch (board.squareState(nextSquare)) {
+                case SquareState.White:
+                    if (this.player === Player.BLACK) availableMoves.push(nextSquare);
+                    break;
+                case SquareState.Black:
+                    if (this.player === Player.WHITE) availableMoves.push(nextSquare);
+                    break;
+                case SquareState.Free:
+                case SquareState.OutOfBounds:
+                case SquareState.King:
+                    break;
+            }
+        }
 
         return availableMoves;
     }
